fea17b6ff63c92571708fe93112a50f698c38671
PERUZZI GIOVANNI <12999427@itisrossi.edu.it>
2024-11-23 09:29:59 +0100

    commit bellissimo

---
+++ script/main.js
@@ -0,0 +1,63 @@
+let operatore = "";
+
+function calcola (value, obj) {
+    switch (value) {
+        case "reset":
+            operatore = "";
+            document.querySelectorAll(".operation:not(#confirm)").forEach ((button) => {
+                button.style.borderColor = "black";
+            });
+            document.getElementById("n1").value = "";
+            document.getElementById("n2").value = "";
+            document.getElementById("operazione").value = "";
+            document.getElementById("risultato").value = "";
+            break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "sqrt":
+        case "power":
+            operatore = value;
+            document.querySelectorAll(".operation:not(#confirm)").forEach ((button) => {
+                button.style.borderColor = "black";
+            });
+            obj.style.borderColor = "red";
+            break;
+        case "=":
+            let ris;
+            let n1 = parseFloat(document.getElementById("n1").value);
+            let n2 = parseFloat(document.getElementById("n2").value);
+            if (operatore == "" || isNaN(n1) || (isNaN(n2) && operatore!="sqrt")) {
+                alert("Input errati");
+            } else {
+                document.querySelectorAll(".operation:not(#confirm)").forEach ((button) => {
+                    button.style.borderColor = "black";
+                });
+                if (operatore == "+") {
+                    ris = n1+n2;
+                } else if (operatore == "-") {
+                    ris = n1-n2;
+                } else if (operatore == "*") {
+                    ris = n1*n2;
+                } else if (operatore == "/") {
+                    if (n2 != 0) {
+                        ris = n1/n2;
+                    } else {
+                        ris = NaN;
+                        alert("Impossibile dividere per 0");
+                    }
+                } else if (operatore == "sqrt") {
+                    ris = (n2==NaN ? Math.pow(n1, 1/n2) : Math.pow(n1, 1/2));
+                } else if (operatore == "power") {
+                    ris = Math.pow(n1, n2);
+                }
+                document.getElementById("n1").value = ris;
+                document.getElementById("n2").value = "";
+                document.getElementById("operazione").value = `${n1} ${operatore} ${isNaN(n2) ?  "" : n2} = ${ris}`;
+                document.getElementById("risultato").value = ris;
+                operatore = "";
+            }
+
+    }
+}
